8ff0c0060ef9599de99bc5ac512e351b5965b5fc
Verônica Rodrigues de Souza <veronica.souza@brgaap.com>
2024-04-24 14:02:12 -0300

    Adicionando o carrousel

---
+++ view/js/home.js
@@ -0,0 +1,16 @@
+const carouselItems = document.querySelectorAll(".carousel-image");
+const proxBtn = document.getElementById("proxBtn");
+const prevBtn = document.getElementById("prevBtn");
+let index = 0;
+
+proxBtn.addEventListener("click", () => {
+    carouselItems[index].classList.add("d-none"); 
+    index = (index + 1) % carouselItems.length; 
+    carouselItems[index].classList.remove("d-none"); 
+});
+
+prevBtn.addEventListener("click", () => {
+    carouselItems[index].classList.add("d-none"); 
+    index = (index - 1 + carouselItems.length) % carouselItems.length; 
+    carouselItems[index].classList.remove("d-none"); 
+});
