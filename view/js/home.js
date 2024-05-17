4dce0d1e58e0e7e1e556d2b984d529d7465ffbc4
Verônica Rodrigues de Souza <veronica.souza@brgaap.com>
2024-05-17 15:51:46 -0300

    Removendo formulario e adicionando novas imagens

--- view/js/home.js
+++ view/js/home.js
@@ -1,18 +1,7 @@
-const carouselItems = document.querySelectorAll(".carousel-image");
-const proxBtn = document.getElementById("proxBtn");
-const prevBtn = document.getElementById("prevBtn");
-let index = 0;
+let index;
 
-proxBtn.addEventListener("click", () => {
-    carouselItems[index].classList.add("d-none"); 
-    index = (index + 1) % carouselItems.length; 
-    carouselItems[index].classList.remove("d-none"); 
-});
-
-prevBtn.addEventListener("click", () => {
-    carouselItems[index].classList.add("d-none"); 
-    index = (index - 1 + carouselItems.length) % carouselItems.length; 
-    carouselItems[index].classList.remove("d-none"); 
-});
-
-fun
+function setSlide() {
+    let divImg = document.querySelectorAll("carousel-image");
+    
+    
+}
