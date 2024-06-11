afa5d37870284329bfe73ee48583e3139bb64d22
Verônica Rodrigues de Souza <veronica.souza@brgaap.com>
2024-06-10 23:05:20 -0300

    Adicionando a primeira função collapse

--- view/js/home.js
+++ view/js/home.js
@@ -5,3 +5,20 @@ function setSlide() {
     
     
 }
+
+function collapse() {
+    const numberPhone = document.querySelector('.number-phone');
+
+    if (numberPhone.classList.contains('show')) {
+        numberPhone.classList.remove('show');
+        numberPhone.style.height = '0'; 
+    } else {
+        numberPhone.style.height = 'auto'; 
+        let height = numberPhone.scrollHeight + 'px'; 
+        numberPhone.style.height = '0'; 
+        setTimeout(() => { 
+            numberPhone.classList.add('show');
+            numberPhone.style.height = height;
+        }, 10);
+    }
+}
